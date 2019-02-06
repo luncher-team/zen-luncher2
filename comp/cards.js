891142b8cc8a99f100c693a8fa1b5f6ac4c2d2aa
x-zen <jdharris.x@gmail.com>
2019-02-05 18:01:22 -0600

    stand-up commit

---
+++ comp/cards.js
@@ -0,0 +1,103 @@
+/*Arroy of Schools*/
+const schools = [{
+  "id": 1,
+  "name": "Harvard University",
+  "motto": "aggregate holistic initiatives",
+  "addr1": "9807 Johnson Drive",
+  "addr2": "Montpellier",
+  "donationsNeeded": "221.49.230.145"
+}, {
+  "id": 2,
+  "name": "Nortre Dame Seishin University",
+  "motto": "leverage back-end users",
+  "addr1": "56 Waxwing Center",
+  "addr2": "Vista Hermosa",
+  "donationsNeeded": "249.226.75.27"
+}, {
+  "id": 3,
+  "name": "Swinburne University of Technology",
+  "motto": "envisioneer front-end applications",
+  "addr1": "1245 Amoth Point",
+  "addr2": "Sertãozinho",
+  "donationsNeeded": "240.131.249.67"
+}, {
+  "id": 4,
+  "name": "Ohio University - Zanesville",
+  "motto": "envisioneer sexy systems",
+  "addr1": "992 Reindahl Circle",
+  "addr2": "Železná Ruda",
+  "donationsNeeded": "30.202.193.160"
+}, {
+  "id": 5,
+  "name": "National Taiwan College of Arts",
+  "motto": "reintermediate value-added relationships",
+  "addr1": "597 Katie Way",
+  "addr2": "Cisiih",
+  "donationsNeeded": "78.136.26.93"
+}, {
+  "id": 6,
+  "name": "Universidad Champagnat",
+  "motto": "benchmark strategic convergence",
+  "addr1": "52217 Mayer Alley",
+  "addr2": "Sīrīs",
+  "donationsNeeded": "106.85.17.193"
+}, {
+  "id": 7,
+  "name": "Escuela Politécnica Nacional",
+  "motto": "productize world-class systems",
+  "addr1": "3030 Independence Center",
+  "addr2": "Zekou",
+  "donationsNeeded": "46.199.65.216"
+}, {
+  "id": 8,
+  "name": "Chongqing University of Technology",
+  "motto": "empower front-end metrics",
+  "addr1": "874 Michigan Lane",
+  "addr2": "Canillo",
+  "donationsNeeded": "126.26.252.110"
+}, {
+  "id": 9,
+  "name": "Instituto Politécnico de Castelo Branco",
+  "motto": "scale next-generation communities",
+  "addr1": "912 Rowland Place",
+  "addr2": "Ar Rujum",
+  "donationsNeeded": "7.168.83.161"
+}, {
+  "id": 10,
+  "name": "Concordia College, St. Paul",
+  "motto": "expedite synergistic technologies",
+  "addr1": "7180 John Wall Drive",
+  "addr2": "Sade",
+  "donationsNeeded": "194.93.139.44"
+}, {
+  "id": 11,
+  "name": "Chongqing Telecommunication College",
+  "motto": "incentivize value-added partnerships",
+  "addr1": "89529 Tennessee Center",
+  "addr2": "Sohbatpur",
+  "donationsNeeded": "73.83.206.31"
+}, {
+  "id": 12,
+  "name": "Osaka Sangyo University",
+  "motto": "synergize real-time solutions",
+  "addr1": "219 Transport Park",
+  "addr2": "Jieshipu",
+  "donationsNeeded": "19.40.81.66"
+}]
+
+/*Object*/
+Card {
+  constructor() {
+    this.card = document.getElementsByClassName('card')
+  }
+
+}
+/*Functions*/
+
+var cards = document.getElementsByClassName('card')
+
+zenCard = schools[i].forEach(
+
+)
+
+}
