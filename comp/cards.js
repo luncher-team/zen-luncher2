f5079f9ac48141d9681e82b6a5909530bc924626
x-zen <jdharris.x@gmail.com>
2019-02-07 16:42:51 -0600

    Pre-Launch Push

--- comp/cards.js
+++ comp/cards.js
@@ -1,96 +1,96 @@
 /*Arroy of Schools*/
 const schools = [{
   "id": 1,
-  "imgSrc": "http://dummyimage.com/400x220.bmp/5fa2dd/ffffff",
-  "name": "Harbin University of Engineering ",
+  "imgSrc": "https://images.unsplash.com/photo-1509062522246-3755977927d7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1404&q=80",
+  "name": "Harbin MS",
   "motto": "drive seamless functionalities",
   "addr1": "0 Superior Parkway",
   "addr2": "New Jersey",
   "donation": "$3785.60"
 }, {
   "id": 2,
-  "imgSrc": "http://dummyimage.com/400x242.jpg/dddddd/000000",
-  "name": "Olivet College",
+  "imgSrc": "https://images.unsplash.com/photo-1508824623134-da66caa91eca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=709&q=80",
+  "name": "Olivet MS",
   "motto": "benchmark synergistic ROI",
   "addr1": "629 Ridge Oak Alley",
   "addr2": "Minnesota",
   "donation": "$9136.73"
 }, {
   "id": 3,
-  "imgSrc": "http://dummyimage.com/400x232.png/cc0000/ffffff",
-  "name": "The Global College Lahore",
-  "motto": "facilitate sexy relationships",
+  "imgSrc": "https://images.unsplash.com/photo-1536337005238-94b997371b40?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  "name": "Global Elementary",
+  "motto": "facilitate strong relationships",
   "addr1": "6 Columbus Alley",
   "addr2": "Missouri",
   "donation": "$2676.07"
 }, {
   "id": 4,
-  "imgSrc": "http://dummyimage.com/400x206.png/ff4444/ffffff",
-  "name": "Chubu Women's College",
+  "imgSrc": "https://images.unsplash.com/photo-1513042966058-0ac1891fdc2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+  "name": "Chubu Women's School",
   "motto": "scale global users",
   "addr1": "47 Fuller Road",
   "addr2": "Ohio",
   "donation": "$3144.68"
 }, {
   "id": 5,
-  "imgSrc": "http://dummyimage.com/400x214.jpg/cc0000/ffffff",
-  "name": "Universidad Católica de Santo Domingo",
+  "imgSrc": "https://images.unsplash.com/photo-1512253077157-2d95165015b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=711&q=80",
+  "name": "Santo Domingo HS",
   "motto": "strategize transparent metrics",
   "addr1": "5567 Bay Circle",
   "addr2": "Oklahoma",
   "donation": "$5381.37"
 }, {
   "id": 6,
-  "imgSrc": "http://dummyimage.com/400x211.png/cc0000/ffffff",
-  "name": "St. Vincent College",
+  "imgSrc": "https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+  "name": "St. Vincent College Prep",
   "motto": "integrate end-to-end networks",
   "addr1": "0355 Erie Road",
   "addr2": "North Carolina",
   "donation": "$731.53"
 }, {
   "id": 7,
-  "imgSrc": "http://dummyimage.com/400x203.jpg/ff4444/ffffff",
-  "name": "Academy of Performing Arts",
+  "imgSrc": "https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80",
+  "name": "HS for Performing Arts",
   "motto": "optimize real-time platforms",
   "addr1": "11 Lotheville Terrace",
   "addr2": "Florida",
   "donation": "$5962.55"
 }, {
   "id": 8,
-  "imgSrc": "http://dummyimage.com/400x216.bmp/5fa2dd/ffffff",
-  "name": "Université de Saida",
+  "imgSrc": "https://images.unsplash.com/photo-1542944951-929275bc9c65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+  "name": "Rudder Elementary",
   "motto": "generate granular platforms",
   "addr1": "34 Old Gate Avenue",
   "addr2": "North Carolina",
   "donation": "$9279.70"
 }, {
   "id": 9,
-  "imgSrc": "http://dummyimage.com/400x223.bmp/cc0000/ffffff",
-  "name": "First Global University",
+  "imgSrc": "https://images.unsplash.com/photo-1524069290683-0457abfe42c3?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  "name": "Standard MS",
   "motto": "morph e-business paradigms",
   "addr1": "9110 Pankratz Street",
   "addr2": "Tennessee",
   "donation": "$2714.26"
 }, {
   "id": 10,
-  "imgSrc": "http://dummyimage.com/400x230.bmp/dddddd/000000",
-  "name": "American Institute of Management)",
+  "imgSrc": "https://images.unsplash.com/photo-1542810634-71277d95dcbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxMTIzfQ&auto=format&fit=crop&w=750&q=80",
+  "name": "Americas HS",
   "motto": "benchmark 24/365 ROI",
   "addr1": "65 Northland Drive",
   "addr2": "California",
   "donation": "$959.12"
 }, {
   "id": 11,
-  "imgSrc": "http://dummyimage.com/400x214.png/dddddd/000000",
-  "name": "Europäische Betriebs-Akademie",
+  "imgSrc": "https://images.unsplash.com/photo-1511629091441-ee46146481b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+  "name": "Jones College Prep",
   "motto": "leverage enterprise eyeballs",
   "addr1": "669 Aberg Trail",
   "addr2": "Idaho",
   "donation": "$6441.57"
 }, {
   "id": 12,
-  "imgSrc": "http://dummyimage.com/400x218.png/dddddd/000000",
-  "name": "University of Technology",
+  "imgSrc": "https://images.unsplash.com/photo-1531496378573-7be09a2697d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+  "name": "MS of Technology",
   "motto": "empower back-end web services",
   "addr1": "28927 Columbus Street",
   "addr2": "Missouri",
